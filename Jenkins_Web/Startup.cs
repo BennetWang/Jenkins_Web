b584c9bdbeca04e450d2a18cb3b436c0c6d8aff6
WDB <690463104@qq.com>
2015-05-22 11:05:16 +0800

    Add Project
    Add Project

---
+++ Jenkins_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jenkins_Web.Startup))]
+namespace Jenkins_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
